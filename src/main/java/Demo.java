0f57aabbef04810715c0e635a926fc5a80562e81
Mouni-IT <164759757+Mouni-IT@users.noreply.github.com>
2024-03-26 22:39:26 +0530

    Update Demo.java

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -2,6 +2,8 @@
 public class Demo extends RuntimeException {
 	public static void main(String[] args) {
 		System.out.println("welcom to maven projrct");
+		System.out.println("welcom to maven projrct");
+		System.out.println("welcom to maven projrct");
 	}
 
 }
