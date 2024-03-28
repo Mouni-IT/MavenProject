1b9074fdf389463524949456666f906a13199de8
mounika <k.mounireddy99@gamil.com>
2024-03-28 22:07:58 +0530

    do method added

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -4,6 +4,11 @@ public class Demo extends RuntimeException {
 		System.out.println("welcom to maven projrct");
 		System.out.println("welcom to maven projrct");
 		System.out.println("welcom to maven projrct");
+
+		public void Domethod{
+
+			System.out.println("hiii");
+
 		
 	}
 
